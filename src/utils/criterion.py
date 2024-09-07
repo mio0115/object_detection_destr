5cdbfcd4f230578fc6e683943f537cd5fdebb85e
daniel <mio0115@github.com>
2024-09-08 02:16:49 +0800

    add get_loss and add background class and corresponding class loss

--- src/utils/criterion.py
+++ src/utils/criterion.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from .bbox_utils import complete_iou
@@ -21,6 +22,31 @@ class SetCriterion(nn.Module):
 
         return total_loss / batch_size
 
+    def _get_ciou_loss(self, pred_boxes, gt_boxes):
+        return self._loss_fns["ciou"](pred_boxes, gt_boxes)
+
+    def _get_bbox_loss(self, pred_boxes, gt_boxes):
+        return self._loss_fns["bbox"](pred_boxes, gt_boxes)
+
+    def _get_class_loss(self, pred_logits, pred_idx, gt_class):
+        selected_objects = pred_logits.index_select(index=pred_idx, dim=0)
+
+        mask = torch.ones(
+            pred_logits.size(0), dtype=torch.bool, device=pred_logits.device
+        )
+        mask[pred_idx] = False
+
+        remaining_objects = pred_logits[mask]
+        ordered_logits = torch.concat([selected_objects, remaining_objects], dim=0)
+
+        objects_num = pred_logits.size(0)
+        dummy_class = torch.tensor([[0, 1]], device=pred_logits.device).expand(
+            (objects_num - pred_idx.size(0), 2)
+        )
+        gt_class = torch.concat([gt_class, dummy_class], dim=0)
+
+        return self._loss_fns["class"](ordered_logits, gt_class)
+
     def forward(self, outputs, targets):
         losses = {"class": 0, "bbox": 0, "ciou": 0}
 
@@ -31,14 +57,14 @@ class SetCriterion(nn.Module):
         for b_output_cls, b_output_box, b_targets, b_idx in zip(
             outputs["pred_class"], outputs["pred_boxes"], targets, indices
         ):
-            output_pred_class = b_output_cls.index_select(index=b_idx[0], dim=0)
             output_pred_boxes = b_output_box.index_select(index=b_idx[0], dim=0)
             gt_class = b_targets["labels"].index_select(index=b_idx[1], dim=0)
             gt_boxes = b_targets["boxes"].index_select(index=b_idx[1], dim=0)
 
-            losses["class"] += self._loss_fns["class"](output_pred_class, gt_class)
-            losses["bbox"] += self._loss_fns["bbox"](output_pred_boxes, gt_boxes)
-            losses["ciou"] += self._loss_fns["ciou"](output_pred_boxes, gt_boxes)
+            losses["class"] += self._get_class_loss(b_output_cls, b_idx[0], gt_class)
+            if b_idx[0].size(0) > 0:
+                losses["bbox"] += self._get_bbox_loss(output_pred_boxes, gt_boxes)
+                losses["ciou"] += self._get_ciou_loss(output_pred_boxes, gt_boxes)
 
         return self._reduce_dict(losses, len(indices))
 
