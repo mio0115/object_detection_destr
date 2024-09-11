4272fd85960d0944fc2abd59526096590d5ec34a
daniel <mio0115@github.com>
2024-09-12 02:39:42 +0800

    add matcher without l1 loss

--- src/utils/matcher.py
+++ src/utils/matcher.py
@@ -112,6 +112,85 @@ class HungarianMatcher(nn.Module):
         ]
 
 
+class HungarianMatcherWoL1(nn.Module):
+    """This class computes an assignment between the targets and the predictions of the network
+    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
+    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
+    while the others are un-matched (and thus treated as non-objects).
+    """
+
+    def __init__(self, cost_class: float = 1, cost_ciou: float = 1):
+        """Creates the matcher
+        Params:
+            cost_class: This is the relative weight of the classification error in the matching cost
+            cost_ciou: This is the relative weight of the giou loss of the bounding box in the matching cost
+        """
+        super().__init__()
+        self.cost_class = cost_class
+        self.cost_ciou = cost_ciou
+        assert cost_class != 0 or cost_ciou != 0, "all costs cant be 0"
+
+    @torch.no_grad()
+    def forward(self, outputs, targets):
+        """Performs the matching
+        Params:
+            outputs: This is a dict that contains at least these entries:
+                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
+                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
+            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
+                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
+                           objects in the target) containing the class labels
+                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
+        Returns:
+            A list of size batch_size, containing tuples of (index_i, index_j) where:
+                - index_i is the indices of the selected predictions (in order)
+                - index_j is the indices of the corresponding selected targets (in order)
+            For each batch element, it holds:
+                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
+        """
+        bs, num_queries, _ = outputs["pred_class"].shape
+
+        # We flatten to compute the cost matrices in a batch
+        out_prob = (
+            outputs["pred_class"].flatten(0, 1).sigmoid()
+        )  # [batch_size * num_queries, num_classes]
+        out_bbox = outputs["pred_boxes"].flatten(0, 1)  # [batch_size * num_queries, 4]
+
+        # Also concat the target labels and boxes
+
+        # turn one-hot encoding labels to normal labels
+        tgt_ids = torch.cat([tgt["labels"] for tgt in targets]).argmax(-1)
+        tgt_bbox = torch.cat([tgt["boxes"] for tgt in targets])
+
+        # Compute the classification cost.
+        alpha = 0.25
+        gamma = 2.0
+        neg_cost_class = (
+            (1 - alpha) * (out_prob**gamma) * (-(1 - out_prob + 1e-8).log())
+        )
+        pos_cost_class = alpha * ((1 - out_prob) ** gamma) * (-(out_prob + 1e-8).log())
+        cost_class = pos_cost_class[:, tgt_ids] - neg_cost_class[:, tgt_ids]
+
+        # Compute the ciou cost betwen boxes
+        cost_ciou = complete_iou(from_cxcyhw_to_xyxy(out_bbox), tgt_bbox)
+
+        # Final cost matrix
+        C = self.cost_class * cost_class + self.cost_ciou * cost_ciou
+        C = C.view(bs, num_queries, -1).cpu()
+
+        sizes = [len(tgt["boxes"]) for tgt in targets]
+        indices = [
+            linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))
+        ]
+        return [
+            (
+                torch.as_tensor(i, dtype=torch.int64),
+                torch.as_tensor(j, dtype=torch.int64),
+            )
+            for i, j in indices
+        ]
+
+
 def build_matcher(args):
     return HungarianMatcher(
         cost_class=args.set_cost_class,
