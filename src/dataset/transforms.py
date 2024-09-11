2af628f913240699b7bf4a4786390fb4bb225a13
daniel <mio0115@github.com>
2024-09-12 02:40:36 +0800

    modify transforms for valid

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -30,8 +30,8 @@ def build_transform(trans_type: TransformTypes):
             [
                 v2.ToImage(),
                 v2.ToDtype(torch.float32, scale=True),
-                v2.Resize(500),
-                v2.CenterCrop(480),
+                v2.Resize(672),
+                v2.CenterCrop(640),
                 v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
         )
