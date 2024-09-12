8c6bc574b33be61a7f340070db40bd3883c292e0
unknown <mio0115@github.com>
2024-09-13 06:44:23 +0800

    add sigmoid_focal_loss

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -51,7 +51,7 @@ def _max_by_axis(the_list):
     return maxes
 
 
-def inverse_sigmoid(tensor, epsilon: float = 1e-7):
+def inverse_sigmoid(tensor, epsilon: float = 1e-6):
     assert epsilon > 0, "epsilon must large than 0"
 
     return -1 * (tensor.clip(min=epsilon).pow(-1) - 1).log()
@@ -87,3 +87,30 @@ def np_softmax(x, axis=-1):
     f_x = y / np.sum(y, axis=axis, keepdims=True)
 
     return f_x
+
+def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: float = 2):
+    """
+    Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+    Args:
+        inputs: A float tensor of arbitrary shape.
+                The predictions for each example.
+        targets: A float tensor with the same shape as inputs. Stores the binary
+                 classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+        alpha: (optional) Weighting factor in range (0,1) to balance
+                positive vs negative examples. Default = -1 (no weighting).
+        gamma: Exponent of the modulating factor (1 - p_t) to
+               balance easy vs hard examples.
+    Returns:
+        Loss tensor
+    """
+    prob = inputs.sigmoid()
+    ce_loss = torch.nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+    p_t = prob * targets + (1 - prob) * (1 - targets)
+    loss = ce_loss * ((1 - p_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        loss = alpha_t * loss
+    
+    return loss.mean(1).sum() / num_boxes
