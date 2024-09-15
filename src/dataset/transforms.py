411a703b7b449551c5784f032e4f7cf92c363a83
daniel <mio0115@github.com>
2024-09-15 11:43:44 +0800

    drop rotation in transforms

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -19,7 +19,6 @@ def build_transform(trans_type: TransformTypes):
                 v2.ToImage(),
                 v2.ToDtype(torch.float32, scale=True),
                 v2.RandomResizedCrop(size=640),
-                v2.RandomRotation(20),
                 v2.RandomHorizontalFlip(0.5),
                 v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
