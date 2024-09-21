541fd6e46684ed8ad9918f21370256f35df4fac5
unknown <mio0115@github.com>
2024-09-21 12:18:52 +0800

    customized train process in train_ssd

--- src/train/train_ssd.py
+++ src/train/train_ssd.py
@@ -7,9 +7,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 from .arg_parser import get_parser
 from ..model.model_ssd import build_model
-from ..dataset.transforms import TransformTypes, build_transform
-from ..dataset.dataset import WiderFace, widerface_collate_fn
-from ..utils.misc import to_device, reduce_dict, resume
+from ..dataset.transforms import TransformTypes, build_transform_ssd
+from ..dataset.dataset import VOCDetection, voc_collate_fn
+from ..utils.misc import to_device, resume
 from ..utils.matcher import build_matcher, SimpleMatcher
 from ..utils.criterion import (
     SSDCriterion,
@@ -23,7 +23,7 @@ def train(
     args,
     model: torch.nn.Module,
     criterion,
-    metric,
+    # metric,
     optimizer: torch.optim.Optimizer,
     train_loader: torch.utils.data.DataLoader,
     valid_loader: torch.utils.data.DataLoader,
@@ -31,110 +31,62 @@ def train(
     writer = SummaryWriter()
 
     model = to_device(model, args.device)
-    loss_weights = {
-        "class": args.set_cost_class,
-        "bbox": args.set_cost_bbox,
-        "ciou": args.set_cost_ciou,
-    }
 
     lowest_vloss, g_step, g_vstep, log_interval = 10000, 0, 0, 100
     for idx in range(args.epochs):
         model.train()
-        loss_model, loss_det, duration, g_step = train_one_epoch(
+        loss, duration, g_step = train_one_epoch(
             args,
             model,
             criterion,
             optimizer=optimizer,
             dataloader=train_loader,
-            loss_weights=loss_weights,
             writer=writer,
             g_step=g_step,
         )
 
-        running_vloss_model, running_vloss_det = 0.0, 0.0
-        running_vloss_class, running_vloss_ciou = 0.0, 0.0
-        prefix_vloss_model, prefix_vloss_det = 0, 0
-        prefix_vloss_class, prefix_vloss_ciou = 0.0, 0.0
+        running_vloss, prefix_vloss = 0.0, 0.0
         model.eval()
 
         with torch.no_grad():
             for vdata in valid_loader:
                 vinputs, vtargets = to_device(vdata, args.device)
 
-                voutputs_model, voutputs_det = model(vinputs)
-                metric(voutputs_model, vtargets)
-                vloss_model = criterion(voutputs_model, vtargets)
-                vloss_det = criterion(voutputs_det, vtargets)
+                voutputs = model(vinputs)
+                # metric(voutputs_model, vtargets)
+                vloss = criterion(voutputs, vtargets)
 
-                running_vloss_class += vloss_model[
-                    "class"
-                ].item() * vinputs.size(0)
-                running_vloss_ciou += vloss_model[
-                    "ciou"
-                ].item() * vinputs.size(0)
-                running_vloss_model += reduce_dict(
-                    vloss_model, weights=loss_weights
-                ).item() * vinputs.size(0)
-                running_vloss_det += reduce_dict(
-                    vloss_det, weights=loss_weights
-                ).item() * vinputs.size(0)
+                running_vloss += vloss.item() * vinputs.size(0)
 
                 g_vstep += 1
                 if g_vstep % log_interval == 0:
-                    avg_vloss_model = (
-                        running_vloss_model - prefix_vloss_model
-                    ) / (log_interval * args.batch_size)
-                    avg_vloss_det = (running_vloss_det - prefix_vloss_det) / (
+                    avg_vloss = (running_vloss - prefix_vloss) / (
                         log_interval * args.batch_size
                     )
-                    avg_vloss_cls = (
-                        running_vloss_class - prefix_vloss_class
-                    ) / (log_interval * args.batch_size)
-                    avg_vloss_ciou = (
-                        running_vloss_ciou - prefix_vloss_ciou
-                    ) / (log_interval * args.batch_size)
 
-                    writer.add_scalar(
-                        "Loss/valid/model", avg_vloss_model, g_vstep
-                    )
-                    writer.add_scalar("Loss/valid/det", avg_vloss_det, g_vstep)
-                    writer.add_scalar(
-                        "Loss/valid/class", avg_vloss_cls, g_vstep
-                    )
-                    writer.add_scalar(
-                        "Loss/valid/ciou", avg_vloss_ciou, g_vstep
-                    )
+                    writer.add_scalar("Loss/valid", avg_vloss, g_vstep)
 
-                    prefix_vloss_det, prefix_vloss_model = (
-                        running_vloss_det,
-                        running_vloss_model,
-                    )
-                    prefix_vloss_class, prefix_vloss_ciou = (
-                        running_vloss_class,
-                        running_vloss_ciou,
-                    )
+                    prefix_vloss = running_vloss
 
-            writer.add_scalar("Metric/mAP", metric.compute(), idx)
-            vloss_model = running_vloss_model / len(valid_loader.dataset)
-            vloss_det = running_vloss_det / len(valid_loader.dataset)
-            metric.reset()
+            # writer.add_scalar("Metric/mAP", metric.compute(), idx)
+            vloss = running_vloss / len(valid_loader.dataset)
 
-        vloss = vloss_model * 0.7 + vloss_det * 0.3
+            # metric.reset()
 
-        if vloss_model < lowest_vloss:
+        if vloss < lowest_vloss:
             torch.save(
                 model.state_dict(),
                 os.path.join("/", "workspace", "checkpoints", args.save_as),
             )
-            lowest_vloss = vloss_model
+            lowest_vloss = vloss
 
         print(
             f"""Epoch {idx+1:>2}: \n\t 
                 Duration: {duration/60:.4f} minutes \n\t
                 Train Loss \n\t\t 
-                    model: {loss_model:.4f} detector: {loss_det:.4f}\n\t 
+                    model: {loss:.4f}\n\t 
                 Valid Loss \n\t\t 
-                    model: {vloss_model:.4f} detector: {loss_det:.4f}"""
+                    model: {vloss:.4f}"""
         )
         writer.close()
 
@@ -145,16 +97,11 @@ def train_one_epoch(
     criterion,
     writer,
     g_step,
-    loss_weights,
     optimizer: torch.optim.Optimizer,
     dataloader: torch.utils.data.DataLoader,
 ):
-    running_loss_det, running_loss_model = 0.0, 0.0
+    running_loss, prefix_loss = 0.0, 0.0
     log_interval = 100
-    prefix_loss_model, prefix_loss_det = 0, 0
-
-    running_loss_class, running_loss_ciou = 0.0, 0.0
-    prefix_loss_class, prefix_loss_ciou = 0.0, 0.0
 
     start_time = time.time()
 
@@ -162,66 +109,36 @@ def train_one_epoch(
         inputs, targets = to_device(data, args.device)
 
         optimizer.zero_grad()
-        model_outputs, det_outputs = model(inputs)
+        outputs = model(inputs)
 
-        loss_model = criterion(model_outputs, targets)
-        loss_det = criterion(det_outputs, targets)
+        loss = criterion(outputs, targets)
 
-        running_loss_class += loss_model["class"].item() * inputs.size(0)
-        running_loss_ciou += loss_model["ciou"].item() * inputs.size(0)
+        running_loss += loss.item() * inputs.size(0)
 
-        total_loss = (
-            reduce_dict(loss_model, weights=loss_weights) * 0.7
-            + reduce_dict(loss_det, weights=loss_weights) * 0.3
-        )
-
-        total_loss.backward()
+        loss.backward()
         optimizer.step()
-
-        running_loss_class += loss_model["class"].item() * inputs.size(0)
-        running_loss_ciou += loss_model["ciou"].item() * inputs.size(0)
-
-        running_loss_model += reduce_dict(
-            loss_model, weights=loss_weights
-        ).item() * inputs.size(0)
-        running_loss_det += reduce_dict(
-            loss_det, weights=loss_weights
-        ).item() * inputs.size(0)
 
         g_step += 1
         if g_step % log_interval == 0:
-            avg_loss_model = (running_loss_model - prefix_loss_model) / (
-                log_interval * args.batch_size
-            )
-            avg_loss_det = (running_loss_det - prefix_loss_det) / (
-                log_interval * args.batch_size
-            )
-            avg_loss_cls = (running_loss_class - prefix_loss_class) / (
-                log_interval * args.batch_size
-            )
-            avg_loss_ciou = (running_loss_ciou - prefix_loss_ciou) / (
+            avg_loss = (running_loss - prefix_loss) / (
                 log_interval * args.batch_size
             )
 
-            writer.add_scalar("Loss/train/model", avg_loss_model, g_step)
-            writer.add_scalar("Loss/train/det", avg_loss_det, g_step)
-            writer.add_scalar("Loss/train/class", avg_loss_cls, g_step)
-            writer.add_scalar("Loss/train/ciou", avg_loss_ciou, g_step)
+            writer.add_scalar("Loss/train/loss", avg_loss, g_step)
+            # writer.add_scalar("Loss/train/class", avg_loss_cls, g_step)
+            # writer.add_scalar("Loss/train/ciou", avg_loss_ciou, g_step)
 
-            prefix_loss_det, prefix_loss_model = (
-                running_loss_det,
-                running_loss_model,
-            )
-            prefix_loss_class, prefix_loss_ciou = (
-                running_loss_class,
-                running_loss_ciou,
-            )
+            prefix_loss = running_loss
 
-    train_loss_model = running_loss_model / len(dataloader.dataset)
-    train_loss_det = running_loss_det / len(dataloader.dataset)
+            # prefix_loss_class, prefix_loss_ciou = (
+            #    running_loss_class,
+            #   running_loss_ciou,
+            # )
+
+    train_loss = running_loss / len(dataloader.dataset)
     end_time = time.time()
 
-    return train_loss_model, train_loss_det, end_time - start_time, g_step
+    return train_loss, end_time - start_time, g_step
 
 
 def test_model(model):
@@ -265,16 +182,16 @@ if __name__ == "__main__":
     # metric = MeanAveragePrecision().to("cpu")
 
     path_to_dataset = os.path.join(os.getcwd(), "dataset")
-    train_ds = WiderFace(
+    train_ds = VOCDetection(
         root=path_to_dataset,
         split=TransformTypes.TRAIN,
-        transform=build_transform(trans_type=TransformTypes.TRAIN),
+        transform=build_transform_ssd(trans_type=TransformTypes.TRAIN),
         augment_factor=5,
     )
-    valid_ds = WiderFace(
+    valid_ds = VOCDetection(
         root=path_to_dataset,
         split=TransformTypes.VALID,
-        transform=build_transform(trans_type=TransformTypes.VALID),
+        transform=build_transform_ssd(trans_type=TransformTypes.VALID),
         augment_factor=1,
     )
 
@@ -282,13 +199,13 @@ if __name__ == "__main__":
         dataset=train_ds,
         batch_size=args.batch_size,
         shuffle=True,
-        collate_fn=widerface_collate_fn,
+        collate_fn=voc_collate_fn,
     )
     valid_dl = torch.utils.data.DataLoader(
         dataset=valid_ds,
         batch_size=args.batch_size,
         shuffle=True,
-        collate_fn=widerface_collate_fn,
+        collate_fn=voc_collate_fn,
     )
 
     train(
